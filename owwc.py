c5c2765b90ba4e27835627055d0e0b65175419ce
Kojo Idrissa <kojo.idrissa@gmail.com>
2018-11-10 19:38:51 -0600

    updated files

--- owwc.py
+++
@@ -1,5 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-ow = requests.get('https://worldcup.playoverwatch.com/en-us/')
-ow_soup = BeautifulSoup(ow.text, 'html.parser')
-print(ow_soup.prettify())
